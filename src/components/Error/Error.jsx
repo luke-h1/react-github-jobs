488b177b91488a9a0988b8c91f9e709bafdc3101
luke-h1 <luke.howsam@yahoo.com>
2020-11-09 19:32:13 +0000

    finish + add state to error component

---
+++ src/components/Error/Error.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Error.scss';
+
+const Error = () => { 
+  return ( 
+    <>
+    <div className="e-container">
+    <h1 className='error-h1'>Enter a valid search term</h1>
+    </div>
+    </>
+  )
+}
+export default Error;
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,11 +1,22 @@
 import React, { useState } from 'react';
 import './Form.scss';
-
+import Error from '../Error/Error';
 const Form = () => {
   const [text, setText] = useState('');
+  const [result, setResult] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
-  const onSubmit = () => {
-    console.log('hello');
+  const onSubmit = (e) => {
+    e.preventDefault(); 
+    if(text === '' || text === null) {
+      setError(true);
+      setTimeout(() => { 
+        setError(false)
+      },1000)
+    }else { 
+
+    }
   };
 
   const onChange = () => {
@@ -16,6 +27,7 @@ const Form = () => {
     <>
       <div className="form-container">
         <div className="form-wrapper">
+          {error ? <Error />   : null}
           <form onSubmit={onSubmit} className="form">
             <input
               type="text"
